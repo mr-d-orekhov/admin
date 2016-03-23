380103b35f01278ae79029008035d2c1e087dab8
Denis Orekhov <do@team-market.ru>
2016-03-23 17:01:00 +0300

    Добавление пробела после значения, если оно есть

--- src/views/default/column/url.blade.php
+++ src/views/default/column/url.blade.php
@@ -1,6 +1,6 @@
 <td>
     @if ( ! is_null($url))
-        <a href="{{ $url }}">{{ $value }}<i class="fa fa-arrow-circle-o-right" data-toggle="tooltip"
-                                            title="{{ trans('admin::lang.table.filter-goto') }}"></i></a>
+        <a href="{{ $url }}">{{ $value?$value.' ': '' }}<i class="fa fa-arrow-circle-o-right" data-toggle="tooltip"
+                                                          title="{{ trans('admin::lang.table.filter-goto') }}"></i></a>
     @endif
 </td>
