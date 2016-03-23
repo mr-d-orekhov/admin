f2bd666265bc90fa33b3d0f10bc47a9f1a0cce59
Denis Orekhov <do@team-market.ru>
2016-03-23 16:53:22 +0300

    Расширение шаблона урла

--- src/SleepingOwl/Admin/Columns/Column/Url.php
+++ src/SleepingOwl/Admin/Columns/Column/Url.php
@@ -5,16 +5,28 @@ use Illuminate\View\View;
 
 class Url extends NamedColumn
 {
+    protected $prefix = '';
 
-	/**
-	 * @return View
-	 */
-	public function render()
-	{
-		$params = [
-			'url'    => $this->getValue($this->instance, $this->name()),
-		];
-		return view(AdminTemplate::view('column.url'), $params);
-	}
+    public function prefix($prefix = null)
+    {
+        if (is_null($prefix)) {
+            return $this->prefix;
+        }
+
+        $this->prefix = $prefix;
+        return $this;
+    }
+
+    /**
+     * @return View
+     */
+    public function render()
+    {
+        $params = [
+            'url'   => $this->prefix() . '/' . $this->getValue($this->instance, $this->name()),
+            'value' => $this->getValue($this->instance, $this->name()),
+        ];
+        return view(AdminTemplate::view('column.url'), $params);
+    }
 
 }
--- src/views/default/column/url.blade.php
+++ src/views/default/column/url.blade.php
@@ -1,3 +1,6 @@
-@if ( ! is_null($url))
-	<a href="{{ $url }}"><i class="fa fa-arrow-circle-o-right" data-toggle="tooltip" title="{{ trans('admin::lang.table.filter-goto') }}"></i></a>
-@endif
+<td>
+    @if ( ! is_null($url))
+        <a href="{{ $url }}"><i class="fa fa-arrow-circle-o-right" data-toggle="tooltip"
+                                title="{{ trans('admin::lang.table.filter-goto') }}"></i></a>
+    @endif
+</td>
